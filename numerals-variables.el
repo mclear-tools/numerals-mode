a4156703eef33acc4c07efcc31d60763817a0aee
Colin McLear <mclear@fastmail.com>
2025-07-28 14:53:00 -0400

    Remove unnecessary dependency tracking code
    Since we switched to save-based updates, we no longer need:
    - Dependency tracking between variables
    - Expression storage for variables
    - Transitive dependency resolution functions
    
    The mode now simply recalculates everything when the file is saved,
    making this complex tracking unnecessary.

--- numerals-variables.el
+++ numerals-variables.el
@@ -18,33 +18,16 @@
 (defvar-local numerals-variables-table nil
   "Hash table storing variable names and their values.")
 
-(defvar-local numerals-variables-dependencies nil
-  "Alist tracking which variables depend on others.
-Each element is (VAR . DEPENDENTS) where DEPENDENTS is a list
-of variables that use VAR in their expressions.")
-
-(defvar-local numerals-variables-expressions nil
-  "Alist storing the original expressions for each variable.
-Each element is (VAR . EXPRESSION).")
 
 (defun numerals-variables-init ()
   "Initialize the variable storage for the current buffer."
-  (setq numerals-variables-table (make-hash-table :test 'equal))
-  (setq numerals-variables-dependencies nil)
-  (setq numerals-variables-expressions nil))
+  (setq numerals-variables-table (make-hash-table :test 'equal)))
 
 (defun numerals-variables-set (name value expression dependencies)
-  "Set variable NAME to VALUE with EXPRESSION and DEPENDENCIES.
-DEPENDENCIES is a list of variable names that this variable depends on."
+  "Set variable NAME to VALUE.
+EXPRESSION and DEPENDENCIES are ignored in save-based mode."
   ;; Store the value
-  (puthash name value numerals-variables-table)
-  
-  ;; Store the expression
-  (setf (alist-get name numerals-variables-expressions nil nil 'equal)
-        expression)
-  
-  ;; Update dependency tracking
-  (numerals-variables-update-dependencies name dependencies))
+  (puthash name value numerals-variables-table))
 
 (defun numerals-variables-get (name)
   "Get the value of variable NAME.
@@ -59,75 +42,14 @@ Returns nil if the variable doesn't exist."
              numerals-variables-table)
     result))
 
-(defun numerals-variables-update-dependencies (var new-deps)
-  "Update dependency tracking for VAR with NEW-DEPS.
-Removes old dependencies and adds new ones."
-  ;; First, remove VAR from all existing dependency lists
-  (dolist (dep-entry numerals-variables-dependencies)
-    (let ((dep-var (car dep-entry))
-          (dependents (cdr dep-entry)))
-      (when (member var dependents)
-        (setf (alist-get dep-var numerals-variables-dependencies nil nil 'equal)
-              (delete var dependents)))))
-  
-  ;; Add VAR to the dependency lists of its new dependencies
-  (dolist (dep new-deps)
-    (let ((dependents (alist-get dep numerals-variables-dependencies nil nil 'equal)))
-      (unless (member var dependents)
-        (setf (alist-get dep numerals-variables-dependencies nil nil 'equal)
-              (cons var dependents))))))
-
-(defun numerals-variables-get-dependents (var)
-  "Get all variables that depend on VAR."
-  (alist-get var numerals-variables-dependencies nil nil 'equal))
-
-(defun numerals-variables-get-expression (var)
-  "Get the original expression for VAR."
-  (alist-get var numerals-variables-expressions nil nil 'equal))
-
 (defun numerals-variables-delete (name)
   "Delete variable NAME and clean up its dependencies."
   ;; Remove from hash table
-  (remhash name numerals-variables-table)
-  
-  ;; Remove from expressions
-  (setq numerals-variables-expressions
-        (assoc-delete-all name numerals-variables-expressions))
-  
-  ;; Clean up dependencies
-  (numerals-variables-update-dependencies name nil)
-  
-  ;; Remove as a dependency source
-  (setq numerals-variables-dependencies
-        (assoc-delete-all name numerals-variables-dependencies)))
-
-(defun numerals-variables-get-all-dependents (var)
-  "Get all variables that transitively depend on VAR.
-Returns a list of variables in dependency order (deepest first)."
-  (let ((visited '())
-        (result '()))
-    (numerals-variables--collect-dependents var visited result)
-    result))
-
-(defun numerals-variables--collect-dependents (var visited result)
-  "Helper function to collect transitive dependents of VAR.
-VISITED tracks already processed variables to avoid cycles.
-RESULT accumulates the dependency-ordered list."
-  (unless (member var visited)
-    (push var visited)
-    (let ((direct-deps (numerals-variables-get-dependents var)))
-      (dolist (dep direct-deps)
-        (numerals-variables--collect-dependents dep visited result))
-      (dolist (dep direct-deps)
-        (unless (member dep result)
-          (push dep result))))
-    result))
+  (remhash name numerals-variables-table))
 
 (defun numerals-variables-clear ()
   "Clear all variables and dependencies."
-  (clrhash numerals-variables-table)
-  (setq numerals-variables-dependencies nil)
-  (setq numerals-variables-expressions nil))
+  (clrhash numerals-variables-table))
 
 (provide 'numerals-variables)
 ;;; numerals-variables.el ends here
